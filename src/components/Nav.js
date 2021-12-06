7a474fc58ab6b674cdc6a92fecb60266b1cbc07a
Axolotlpi <cfc3.1415@gmail.com>
2021-12-05 22:02:26 -0600

    fix typo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,8 +3,8 @@ function Nav() {
   return (
     <nav>
         <div className="nav-wrapper cyan lighten-2">
-        <a href="#" class="brand-logo">WebMote</a>
-        <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <a href="#" className="brand-logo">WebMote</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
             <li><a href="http://www.google.com" target="_blank">this1</a></li>
             <li>this2</li>
             <li>this3</li>
