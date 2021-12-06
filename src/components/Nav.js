c3ed7609c2853ae61b7d92d53ee46f8620ccb665
Axolotlpi <cfc3.1415@gmail.com>
2021-12-05 20:59:06 -0600

    add Nav

---
+++ src/components/Nav.js
@@ -0,0 +1,17 @@
+
+function Nav() {
+  return (
+    <nav>
+        <div className="nav-wrapper cyan lighten-2">
+        <a href="#" class="brand-logo">WebMote</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="http://www.google.com" target="_blank">this1</a></li>
+            <li>this2</li>
+            <li>this3</li>
+        </ul>
+        </div>
+    </nav>
+  );
+}
+
+export default Nav;
