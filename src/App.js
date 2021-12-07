be536970a4d2c65569c4bb19a57dba69405053e6
Axolotlpi <cfc3.1415@gmail.com>
2021-12-06 19:38:12 -0600

    separate editor

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import Nav from "./components/Nav";
 import Grid from "./components/Grid";
 import ActionButton from "./compositions/ActionButton";
 import { readActionsFromStore, writeActionsToStore } from "./helpers/Actions";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Editor from "./compositions/Editor";
 
 function App() {
-  
   const [actions, setActions] = useState([]);
-  
+
   useEffect(() => {
     setActions(readActionsFromStore());
   }, []);
@@ -17,6 +17,13 @@ function App() {
     writeActionsToStore(actions);
   }, [actions]);
 
+  const setSingleAction = (index, action) => {
+    let newActions = actions;
+    newActions.splice(index, 1, action);
+    setActions(newActions);
+  }
+
+  let buttons = actions.map((action) => <ActionButton key={action.id} action={action} />);
 
   return (
     <Router>
@@ -24,7 +31,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/remote" element={<Grid items={buttons} />} />
-          <Route path="/editor" element={<Grid />} />
+          <Route path="/editor" element={<Editor actions={actions} setSingleAction={setSingleAction} />} />
         </Routes>
       </div>
     </Router>
