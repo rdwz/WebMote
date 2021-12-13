79f34b019e9a13c19eebedf5ab896409b6be4bf6
Axolotlpi <cfc3.1415@gmail.com>
2021-12-13 00:58:24 -0600

    change head link

--- src/components/Head.js
+++ src/components/Head.js
@@ -5,7 +5,7 @@ function Head(props) {
     <>
       <nav>
           <div className="nav-wrapper cyan lighten-2">
-          <a href="#" className="brand-logo">{props.title}</a>
+          <a href="/" className="brand-logo">{props.title}</a>
           </div>
       </nav>
     </>
